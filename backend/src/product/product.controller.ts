9f33b642f7081b53a9ff198fe6b03ad1ba8e49ce
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-02 01:06:10 +0700

    feat(product): get from db

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,14 +1,23 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
 
 @Controller('products')
 export class ProductController {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
   @Get()
   getAll() {
-    return 'all products';
+    console.log('hello');
+    return this.productRepository.find();
   }
 
   @Get(':id')
   getOne(@Param('id') id: number) {
-    return 'get one product';
+    return this.productRepository.findOne(id);
   }
 }
--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './product.controller';
+import { Product } from './product.entity';
 
 @Module({
-  controllers: [ProductController]
+  imports: [TypeOrmModule.forFeature([Product])],
+  controllers: [ProductController],
 })
 export class ProductModule {}
