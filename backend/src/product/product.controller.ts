e726ddfb44cc1cdc5a1111825c4f80a5eb71ece3
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-04 16:36:06 +0700

    feat: get product types and brands

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,17 +1,37 @@
+import { ProductBrand } from './entities/product-brand.entity';
 import { Controller, Get, Param } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ProductRepository } from './product.repository';
+import { Repository } from 'typeorm';
+import { ProductType } from './entities/product-type.entity';
 
 @Controller('products')
 export class ProductController {
-  constructor(private readonly productRepository: ProductRepository) {}
+  constructor(
+    private readonly productRepository: ProductRepository,
+    @InjectRepository(ProductBrand)
+    private readonly productBrandRepository: Repository<ProductBrand>,
+    @InjectRepository(ProductType)
+    private readonly productTypeRepository: Repository<ProductType>,
+  ) {}
 
   @Get()
-  getAll() {
+  getAllProducts() {
     return this.productRepository.getProducts();
   }
 
+  @Get('brands')
+  getProductBrands() {
+    return this.productBrandRepository.find();
+  }
+
+  @Get('types')
+  getProductTypes() {
+    return this.productTypeRepository.find();
+  }
+
   @Get(':id')
-  getOne(@Param('id') id: number) {
+  getOneProduct(@Param('id') id: number) {
     return this.productRepository.getProductById(id);
   }
 }
--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -1,10 +1,14 @@
+import { ProductBrand } from './entities/product-brand.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductType } from './entities/product-type.entity';
 import { ProductController } from './product.controller';
 import { ProductRepository } from './product.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductRepository])],
+  imports: [
+    TypeOrmModule.forFeature([ProductRepository, ProductType, ProductBrand]),
+  ],
   controllers: [ProductController],
 })
 export class ProductModule {}
