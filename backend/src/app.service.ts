ac5ee72726967276f34fe9321c886fda548faf5b
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-04 16:53:11 +0700

    chore: log seeding

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -18,13 +18,16 @@ export class AppService implements OnApplicationBootstrap {
 
     try {
       if ((await productBrandRepo.count()) === 0) {
+        console.log(`seending brands...`);
         await productBrandRepo.insert(brands as any);
       }
 
       if ((await productTypeRepo.count()) === 0) {
+        console.log(`seeding types...`);
         await productTypeRepo.insert(types as any);
       }
       if ((await productRepo.count()) === 0) {
+        console.log(`seeding products...`);
         await productRepo.insert(products as any);
       }
     } catch (e) {
