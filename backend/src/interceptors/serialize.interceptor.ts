fc4ed4d9ac574b8a5fa3aa66a981c24cdd2f9688
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-19 21:43:36 +0700

    feat(order: serialize order by dto

--- backend/src/interceptors/serialize.interceptor.ts
+++ backend/src/interceptors/serialize.interceptor.ts
@@ -12,59 +12,96 @@ interface ClassContructor {
   new (...args: any[]): {};
 }
 
-export function Serialize(
-  dto: ClassContructor | Map<ClassContructor, ClassContructor>,
-) {
+export function Serialize(dto: ClassContructor | Map<any, any>) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 @Injectable()
 export class SerializeInterceptor implements NestInterceptor {
-  constructor(
-    private dtoOrMapping:
-      | ClassContructor
-      | Map<ClassContructor, ClassContructor>,
-  ) {}
+  private json: any;
+
+  constructor(private dtoOrMapping: ClassContructor | Map<any, any>) {
+    this.json = {};
+  }
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data: any) => {
         // TODO: refactor to be more intuitive
-        if (this.dtoOrMapping.constructor.name === 'Map') {
-          return this.serializeRecursively(data);
-        } else {
-          return plainToInstance(this.dtoOrMapping as ClassContructor, data, {
-            excludeExtraneousValues: true,
-          });
-        }
+        // if (this.dtoOrMapping.constructor.name === 'Map') {
+        //   this.json = JSON.parse(JSON.stringify(data));
+        //   return this.serializeRecursively(data);
+        // }
+
+        // example: @Serialize(ProductDto)
+        // serialize from data to ProductDto
+        return plainToInstance(this.dtoOrMapping as ClassContructor, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
 
   private serializeRecursively(value: any): any {
-    this.dtoOrMapping = this.dtoOrMapping as Map<
-      ClassContructor,
-      ClassContructor
-    >; /* fix type error */
+    this.dtoOrMapping = this.dtoOrMapping as Map<any, any>; /* fix type error */
 
     if (Array.isArray(value)) {
       return value.map((i) => this.serializeRecursively(i));
     }
+
+    // if (this.dtoOrMapping.has(value?.constructor)) {
+    //   return plainToInstance(this.dtoOrMapping.get(value.constructor), value, {
+    //     excludeExtraneousValues: true,
+    //   });
+    // }
+
+    // if (value !== null && typeof value === 'object') {
+    //   return Object.fromEntries(
+    //     Object.entries(value).map(([key, value]) => [
+    //       key,
+    //       this.serializeRecursively(value),
+    //     ]),
+    //   );
+    // }
+
+    // return value;
+
+    // Case 1: object with existing type
+    let obj: any = value;
+
     if (this.dtoOrMapping.has(value?.constructor)) {
-      return plainToInstance(this.dtoOrMapping.get(value.constructor), value, {
-        excludeExtraneousValues: true,
-      });
-    }
+      const destType = this.dtoOrMapping.get(value.constructor);
+      obj = plainToInstance(destType, value, { excludeExtraneousValues: true });
 
-    if (value !== null && typeof value === 'object') {
-      return Object.fromEntries(
-        Object.entries(value).map(([key, value]) => [
-          key,
-          this.serializeRecursively(value),
-        ]),
-      );
+      for (let key in obj) {
+        if (key in value) {
+          obj[key] = this.serializeRecursively(value[key]);
+        }
+      }
     }
+    // Case 2: object with non-existing type
+    // Case 3: array with existing type
+    // Case 4: array with non-existing type
 
-    return value;
+    // Case 5: primitive value (string, number, boolean)
+    return obj;
   }
 }
+
+`
+  Order {
+    name: 'abc',
+    type: 'xyz',
+    secret: 'xxx',
+    orderItems: [
+      OrderItem: {
+        id: 1,
+        date: '2012-02-15',
+        price: 123,
+        stuff: 'mnp'
+      }
+    ]
+  }
+
+  =>
+`;
---
+++ backend/src/order/dtos/order.dto.ts
@@ -0,0 +1,44 @@
+import { Expose, Transform, Type } from 'class-transformer';
+import { OrderItemDto } from './order-item.dto';
+export class OrderDto {
+  @Expose()
+  buyerEmail: string;
+
+  @Expose()
+  orderDate: Date;
+
+  @Expose()
+  subTotal: number;
+
+  @Expose()
+  total: number;
+
+  @Expose()
+  status: string;
+
+  @Expose()
+  shipToFirstName: string;
+
+  @Expose()
+  shipToLastName: string;
+
+  @Expose()
+  shipToStreet: string;
+
+  @Expose()
+  shipToCity: string;
+
+  @Expose()
+  shipToState: string;
+
+  @Expose()
+  shipToZipCode: string;
+
+  @Expose()
+  @Transform(({ obj }) => obj.deliveryMethod.shortName)
+  deliveryMethod: string;
+
+  @Expose()
+  @Type(() => OrderItemDto)
+  orderItems: OrderItemDto[];
+}
