6fc317b87b343a04d692205527d203463538b481
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-19 20:28:29 +0700

    feat(auth): expires in 24h

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -21,7 +21,7 @@ import { JwtStrategy } from './jwt.strategy';
       useFactory: () => ({
         secret: process.env.AUTH_SECRET,
         signOptions: {
-          expiresIn: '60m',
+          expiresIn: '24h',
         },
       }),
     }),
