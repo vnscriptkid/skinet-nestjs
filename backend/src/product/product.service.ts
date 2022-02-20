5b44d3120a262ca7aa4b0b03c72df79c2438f436
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-20 19:54:44 +0700

    feat(product): find ids throw

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
@@ -11,5 +11,13 @@ export class ProductService {
   ) {}
   findByIds(ids: number[]) {
     return this.productRepository.findByIds(ids);
+  }
+
+  async findByIdsThrow(ids: number[]) {
+    const products = await this.findByIds(ids);
+
+    if (ids.length !== products.length) throw new NotFoundException();
+
+    return products;
   }
 }
