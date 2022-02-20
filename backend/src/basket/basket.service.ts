a734b2aece53df2f09d949774990af52e9084e8f
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-20 19:57:30 +0700

    feat(basket): get basket throw

--- backend/src/basket/basket.service.ts
+++ backend/src/basket/basket.service.ts
@@ -6,20 +6,25 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { Cache } from 'cache-manager';
+import { UpdateBasketDto } from './dtos/update-basket.dto';
 
 @Injectable()
 export class BasketService {
   constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
   async getBasket(id: string) {
-    const basket = await this.cacheManager.get<Basket>(id);
+    return await this.cacheManager.get<Basket>(id);
+  }
+
+  async getBasketThrow(id: string) {
+    const basket = await this.getBasket(id);
 
     if (!basket) throw new NotFoundException();
 
     return basket;
   }
 
-  async updateBasket(basket: Basket) {
+  async updateBasket(basket: UpdateBasketDto) {
     await this.cacheManager.set(basket.id, basket, {
       ttl: 1000 * 3600 * 24 * 30,
     }); // ttl: 30days
